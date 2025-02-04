e0227d0cb2f15a10b6cb37d1d7a7aabbb0f3cf16
proskurekov <proskurekovdim@gmail.com>
2025-02-04 16:02:06 +0700

    add recover in config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 var (
@@ -15,4 +16,6 @@ func SetupConfigs(app *fiber.App) {
 		Format:     "${time} ${method} ${path} - ${status} - ${latency}\n",
 		TimeFormat: "2006-01-02 15:04:05.000000",
 	}))
+
+	app.Use(recover.New())
 }
